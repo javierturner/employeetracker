bc05c25d6a8672ece101fd09d71aa981ea865804
JT Turner <j.tturner14@yahoo.com>
2020-01-17 18:17:30 -0500

    finished sql page, inserted values

--- database.sql
+++ database.sql
@@ -23,3 +23,18 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT 
 );
+
+
+INSERT INTO department (name) VALUES 
+    ('Sales'), 
+    ('Estimating'), 
+    ('Overhead');
+
+INSERT INTO role (title, salary, department_id) VALUE 
+    ('Receptionist', $35,000, 'Overhead'), 
+    ('Production Estimator', $30,900, 'Estimating'), 
+    ('Customer Service Representative', $40,000, 'Sales');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+    ('Jenni', 'Rams', 1, 1),
+    ('Ann', 'Cabana', 2, 2);
