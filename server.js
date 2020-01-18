741da2f8a635737e331ad0c3a4f25ca49736a0bd
JT Turner <j.tturner14@yahoo.com>
2020-01-17 19:46:36 -0500

    added initial inqirer questions

--- server.js
+++ server.js
@@ -21,5 +21,63 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // run the start function after the connection is made to prompt the user
-    // start();
+    start();
 });
+
+//start inquirer prompting
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "View All Roles",
+            "Done"
+        ]
+    })
+        .then(function (answer) {
+            switch (data.answer) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "View All Employees by Department":
+                    viewEmpByDept();
+                    break;
+
+                case "View All Employees by Manager":
+                    viewEmpByMan();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmpRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmpManager();
+                    break;
+
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+
+                case "Exit Application":
+                    connection.end();
+                    break;
+            }})
+}
